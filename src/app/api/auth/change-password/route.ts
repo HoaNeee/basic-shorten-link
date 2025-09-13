d0538e8cda8fd0dad7b0dedc49acb0f435707b74
hoa-computer <nhhoa03@gmail.com>
2025-09-13 21:29:06 +0700

    fix next again

--- src/app/api/auth/change-password/route.ts
+++ src/app/api/auth/change-password/route.ts
@@ -4,46 +4,46 @@ import { getPayloadWithJWT, verifyPassword } from "@/lib/utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export const PATCH = errorHandler(async (req: NextRequest) => {
-  const body = await req.json();
+	const body = await req.json();
 
-  const { currentPassword, newPassword, confirmPassword, id } = body as {
-    currentPassword: string;
-    newPassword: string;
-    confirmPassword: string;
-    id: number;
-  };
+	const { currentPassword, newPassword, confirmPassword, id } = body as {
+		currentPassword: string;
+		newPassword: string;
+		confirmPassword: string;
+		id: number;
+	};
 
-  if (newPassword.trim() !== confirmPassword.trim()) {
-    throw new ApiError(400);
-  }
+	if (newPassword.trim() !== confirmPassword.trim()) {
+		throw new ApiError(400);
+	}
 
-  const payload = await getPayloadWithJWT(req);
+	const payload = await getPayloadWithJWT(req);
 
-  if (payload?.id !== id) {
-    throw new ApiError(403);
-  }
+	if (payload?.id !== id) {
+		throw new ApiError(403);
+	}
 
-  const user = await getUserWithId(id);
+	const user = await getUserWithId(id);
 
-  const isMatch = await verifyPassword(
-    currentPassword,
-    user.password as string
-  );
+	const isMatch = await verifyPassword(
+		currentPassword,
+		user?.password as string
+	);
 
-  if (!isMatch) {
-    throw new ApiError(400, "Current passowrd is not correct.");
-  }
+	if (!isMatch) {
+		throw new ApiError(400, "Current passowrd is not correct.");
+	}
 
-  await updateUserWithId(
-    {
-      password: newPassword,
-    },
-    id
-  );
+	await updateUserWithId(
+		{
+			password: newPassword,
+		},
+		id
+	);
 
-  return NextResponse.json({
-    success: true,
-    message: "Success!",
-    data: {},
-  });
+	return NextResponse.json({
+		success: true,
+		message: "Success!",
+		data: {},
+	});
 });
