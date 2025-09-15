e8ee2a89bc28b3d8ae3aa794258d1f4cb329de5b
hoa-computer <nhhoa03@gmail.com>
2025-09-15 14:33:49 +0700

    fix connect

--- src/lib/db/connect.ts
+++ src/lib/db/connect.ts
@@ -2,8 +2,9 @@ import mysql2 from "mysql2/promise";
 import { isProduction } from "../contant";
 
 export const pool = mysql2.createPool({
-  host: isProduction ? process.env.DB_HOST : "localhost",
-  user: isProduction ? process.env.DB_USER : "root",
-  password: isProduction ? process.env.DB_PASSWORD : "",
-  database: process.env.DB_NAME,
+	host: isProduction ? process.env.DB_HOST : "localhost",
+	user: isProduction ? process.env.DB_USER : "root",
+	password: isProduction ? process.env.DB_PASSWORD : "",
+	port: isProduction ? Number(process.env.DB_PORT) : 3306,
+	database: process.env.DB_NAME,
 });
